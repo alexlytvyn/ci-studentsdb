9c4636a93afc3f13377ab54f2153beedac2b1d14
Alexander Lytvyn <alex.lytvyn.13@gmail.com>
2017-08-17 09:02:03 +0300

    Initial commit.

---
+++ application/controllers/Groups.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Groups extends CI_Controller
+{
+    public function view($page = 'groups')
+    {
+        if (! file_exists(APPPATH.'views/templates/'.$page.'.php')) {
+            // Whoops, we don't have a page for that!
+                        show_404();
+        }
+
+        $this->load->view('templates/'.$page);
+    }
+}
---
+++ application/controllers/Journal.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Journal extends CI_Controller
+{
+    public function view($page = 'journal')
+    {
+        if (! file_exists(APPPATH.'views/templates/'.$page.'.php')) {
+            // Whoops, we don't have a page for that!
+                        show_404();
+        }
+
+        $this->load->view('templates/'.$page);
+    }
+}
